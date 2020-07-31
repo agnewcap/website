311d04682e58a15b079bae0b50b32e2607f027e5
Bailey <bailey@agnewcap.com>
2020-07-30 19:25:26 -0500

    tried to integrate bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Image from './baileyandsammy.png';
 import './App.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 
 
 function App() {
   return (
     <section className="App">
-      <div class={Jumbotron}>
-        <h1>I love you Sammy!</h1>
-        <p className="App"><img src={Image}/></p>
+      <div>
+        <h1>I love you Sammy</h1>
+        <p className="App"><img src={Image}></img></p>
       </div>
+      
     </section>
-  )
+  );
 }
 
 export default App;
