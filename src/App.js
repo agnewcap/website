c6969869a1cfe5c98b0ad994866c997049963082
Bailey <bailey@agnewcap.com>
2020-07-30 19:19:21 -0500

    for Sammy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Image from './baileyandsammy.png';
 import './App.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>I love you Sammy <3</h1>
-    </div>
-  );
+    <section className="App">
+      <div class={Jumbotron}>
+        <h1>I love you Sammy!</h1>
+        <p className="App"><img src={Image}/></p>
+      </div>
+    </section>
+  )
 }
 
 export default App;
